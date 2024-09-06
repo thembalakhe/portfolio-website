df6ba48677295a9dddf41caded12f78ffa8aee1d
thembalakhe <m.themba1738@gmail.com>
2024-09-06 23:27:18 +0200

    created page routes and updated all the pages with content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/Pages/home/home.component';
+import { PortfolioComponent } from './components/Pages/portfolio/portfolio.component';
+import { AboutComponent } from './components/Pages/about/about.component';
+import { ContactComponent } from './components/Pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: HomeComponent},
+];
